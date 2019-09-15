4b311c6c235a2aa05b204c95cf8ae8ed0fda5faa
Shilton Naicker (ZA) <Shilton.Naicker@absa.co.za>
2019-09-16 01:17:35 +0200

    saveVersion.groovy edited online with Bitbucket

--- vars/saveVersion.groovy
+++ vars/saveVersion.groovy
@@ -79,7 +79,7 @@ def writeNewDeploymentConfig(input, filename){
 
 
 def process(String country, String env) {
-    final String FILE_NAME = "../../../../../../output.json"
+    final String FILE_NAME = "/var/lib/jenkins/jobs/newpipe/output.json"
     try {
         // Read File
         DeploymentConfiguration input = readDeploymentConfiguration(FILE_NAME)
